d9ee3247375c1661eeff5a0d13cf49e381c3efcf
ChrisDTon <christer.daniel@gmail.com>
2023-02-10 14:01:10 +0100

    Updated navigator

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,15 @@
 export default function Navbar() {
   return (
-    <header className="bg-blue-900 p-10 mb-5">
-      <h1>EliteSerien</h1>
+    <header className="bg-blue-900 h-36 mb-5">
+      <div className="float-right mr-40">
+        <div>
+          <p className="text-right mb-2 p-2 text-lg hover:bg-white hover:text-black float-right font-medium">Login</p>
+          <p className="text-right mb-2 p-2 text-lg hover:bg-white hover:text-black float-right font-medium">Bytt til tidligere avstemmninger</p>
+        </div>
+        
+        <p className="bg-sky-400 text-white font-bold py-2 px-4 float-right border-4 text-3xl">ALLE AVSTEMMNINGER</p>
+      </div>
+      <h1 className="pt-12 ml-40 w-24 text-3xl font-semibold">EliteSerien</h1>
     </header>
   );
 }

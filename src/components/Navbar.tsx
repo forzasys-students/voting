eafd4e6ee17ca4c3dff54fbcea126b232ee071d4
ChrisDTon <christer.daniel@gmail.com>
2023-02-13 12:58:24 +0100

    Navbar buttons navigation except login

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,13 @@
+import Link from "next/link";
 export default function Navbar() {
   return (
     <header className="bg-blue-900 h-36 mb-5">
       <div className="float-right mr-40">
         <div>
-          <p className="text-right mb-2 p-2 text-lg hover:bg-white hover:text-black float-right font-medium cursor-pointer" onClick={() => alert("Chnage to login page")}>Login</p>
-          <p className="text-right mb-2 p-2 text-lg hover:bg-white hover:text-black float-right font-medium cursor-pointer" onClick={() => alert("Change to overview page of old votes")}>Bytt til tidligere avstemmninger</p>
+          <Link href="/poll" className="text-right mb-2 p-2 text-lg hover:bg-white hover:text-black float-right font-medium cursor-pointer">Login</Link>
+          <Link href="/" className="text-right mb-2 p-2 text-lg hover:bg-white hover:text-black float-right font-medium cursor-pointer">Bytt til tidligere avstemmninger</Link>
         </div>
-        
-        <p className="bg-sky-400 text-white font-bold py-2 px-4 float-right border-4 text-3xl cursor-pointer" onClick={() => alert("Chnage to overview page")}>ALLE AVSTEMMNINGER</p>
+        <Link href="/" className="bg-sky-400 text-white font-bold py-2 px-4 float-right border-4 text-3xl cursor-pointer">ALLE AVSTEMMNINGER</Link>
       </div>
       <h1 className="pt-12 ml-40 w-24 text-3xl font-semibold cursor-default">EliteSerien</h1>
     </header>

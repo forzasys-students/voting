0ddd93cd79f4722bccd1d2b914ed18dfbe84dc5c
ewenjo <github@ewenjo.dev>
2023-02-13 13:07:55 +0100

    Admin: Matchdata fetching

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,12 +1,38 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
+
+import type { Playlist, Response } from "@/types/matchdata";
 
 export default function Admin() {
+  const [events, setEvents] = useState<Playlist[]>([]);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("/api/matchdata")
+      .then((res) => res.json())
+      .then((data: Response) => {
+        setEvents(data.playlists);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <>
       <Head>
         <title>Admin</title>
       </Head>
-      <div>Admin</div>
+      <h3 className="text-2xl font-bold">Events</h3>
+      {events.map((event) => {
+        return (
+          <div key={event.id} className="py-2">
+            <h4 className="text-1xl">{event.description}</h4>
+            <p className="text-gray-400">
+              {new Date(event.date).toLocaleString()}
+            </p>
+          </div>
+        );
+      })}
     </>
   );
 }

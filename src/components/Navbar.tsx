be054ab27b23a7cc1db9ce386921ed0ecb616e96
ChrisDTon <christer.daniel@gmail.com>
2023-02-23 16:40:21 +0100

    Screen size adjustment and absolute

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,16 @@
 import Link from "next/link";
 export default function Navbar() {
   return (
-    <header className="bg-blue-900 h-36 mb-5">
+    <header className="bg-[#10253E] h-36 mb-5">
       <div className="float-right mr-40">
         <div>
-          <Link href="/poll" className="text-right mb-2 p-2 text-lg hover:bg-white hover:text-black float-right font-medium cursor-pointer">Login</Link>
-          <Link href="/" className="text-right mb-2 p-2 text-lg hover:bg-white hover:text-black float-right font-medium cursor-pointer">Bytt til tidligere avstemmninger</Link>
-          <Link href="/" className="text-right mb-2 p-2 text-lg hover:bg-white hover:text-black float-right font-medium cursor-pointer">Poll</Link>
+          <Link href="/poll" className="text-right mb-2 p-2 text-lg text-white hover:bg-white hover:text-black float-right font-medium cursor-pointer">Login</Link>
+          <Link href="/" className="text-right mb-2 p-2 text-lg text-white hover:bg-white hover:text-black float-right font-medium cursor-pointer">Bytt til tidligere avstemmninger</Link>
+          <Link href="/poll" className="text-right mb-2 p-2 text-lg text-white hover:bg-white hover:text-black float-right font-medium cursor-pointer">Poll</Link>
         </div>
         <Link href="/" className="bg-sky-400 text-white font-bold py-2 px-4 float-right border-4 text-3xl cursor-pointer">ALLE AVSTEMMNINGER</Link>
       </div>
-      <h1 className="pt-12 ml-40 w-24 text-3xl font-semibold cursor-default">EliteSerien</h1>
+      <h1 className="pt-12 ml-40 w-24 text-3xl text-black outline-white outline-8 font-semibold cursor-default">EliteSerien</h1>
     </header>
   );
 }
--- src/pages/poll/index.tsx
+++ src/pages/poll/index.tsx
@@ -1,3 +1,35 @@
 export default function Poll() {
-  return <div>Poll</div>;
+  return (
+    <>
+    <div>
+      <h1 className="font-semibold text-4xl">Poll Title</h1>
+      <h2 className="font-regular">Poll Flavor Text</h2>
+      <p className="font-light">x votes</p>
+    </div>
+    <div>
+      <div className="bg-[#f2f2f2] flex lg:h-64 md:h-32 relative">
+        <video controls className="lg:h-64 md:h-32 absolute"></video>
+        <div className="lg:left-96 lg:ml-32 md:left-48 md:ml-16 absolute">
+          <div className="flex lg:mt-6 lg:ml-6 md:mt-3 md:ml-3 sm:mt-1 sm:ml-1">
+            <p className="font-light">Event date</p>
+            <p className="mr-2 ml-2 font-light"> | </p>
+            <p className="font-light">Video length</p>
+          </div>
+          <div className="lg:mt-14 lg:ml-6 md:mt-4 md:ml-3 sm:mt-2 sm:ml-1">
+            <p className="font-medium text-3xl">Video Title</p>
+          </div>
+          <div className="lg:mt-16 lg:ml-6 md:mt-4 md:ml-3 sm:mt-3 sm:ml-2">
+            <p className="font-light">Video origin</p>
+          </div>
+        </div>
+        <div className="absolute right-16 top-28">
+          <p className="text-3xl">0%</p>
+        </div>
+        <div className="absolute left-1/3 ml-3 bg-sky-400 w-2/3 overflow-hidden">
+          <p>pinal</p>
+        </div>
+      </div>
+    </div>
+    </>
+  );
 }

cf885ec3b72a427e83a7dba74c72307c5c1bd8b2
ChrisDTon <christer.daniel@gmail.com>
2023-02-24 11:59:19 +0100

    Updated Nav with correct redirect and positioning

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,16 @@
 import Link from "next/link";
 export default function Navbar() {
   return (
-    <header className="bg-[#10253E] h-36 mb-5">
-      <div className="float-right mr-40">
+    <header className="bg-[#10253E] h-36 mb-5 static">
+      <div className="right-0">
         <div>
-          <Link href="/poll" className="text-right mb-2 p-2 text-lg text-white hover:bg-white hover:text-black float-right font-medium cursor-pointer">Login</Link>
-          <Link href="/" className="text-right mb-2 p-2 text-lg text-white hover:bg-white hover:text-black float-right font-medium cursor-pointer">Bytt til tidligere avstemmninger</Link>
-          <Link href="/poll" className="text-right mb-2 p-2 text-lg text-white hover:bg-white hover:text-black float-right font-medium cursor-pointer">Poll</Link>
+          <Link href="/login" className="text-right mb-2 p-2 text-lg text-white hover:bg-white hover:text-black right-40 font-medium cursor-pointer absolute">Login</Link>
+          <Link href="/" className="text-right mb-2 p-2 text-lg text-white hover:bg-white hover:text-black right-40 mr-16 font-medium cursor-pointer absolute">Bytt til tidligere avstemmninger</Link>
+          <Link href="/poll" className="text-right mb-2 p-2 text-lg text-white hover:bg-white hover:text-black left-96 font-medium cursor-pointer absolute">Poll</Link>
         </div>
-        <Link href="/" className="bg-sky-400 text-white font-bold py-2 px-4 float-right border-4 text-3xl cursor-pointer">ALLE AVSTEMMNINGER</Link>
+        <Link href="/overview" className="bg-sky-400 text-white font-bold py-2 px-4 border-4 text-3xl right-40 top-12 cursor-pointer absolute">ALLE AVSTEMMNINGER</Link>
       </div>
-      <h1 className="pt-12 ml-40 w-24 text-3xl text-black outline-white outline-8 font-semibold cursor-default">EliteSerien</h1>
+      <h1 className="pt-12 w-24 text-3xl text-black outline-white outline-8 left-40 font-semibold cursor-default absolute">EliteSerien</h1>
     </header>
   );
 }

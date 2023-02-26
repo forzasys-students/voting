32b5e564e6a5e9219c21827a79290f1fa6ee6b9c
ChrisDTon <christer.daniel@gmail.com>
2023-02-26 17:47:53 +0100

    Updated view poll / voting layout

--- src/pages/poll/index.tsx
+++ src/pages/poll/index.tsx
@@ -22,26 +22,26 @@ export default function Poll() {
         <p className="font-light">{data.votes} votes</p>
       </div>
       <div>
-        <div className="bg-[#f2f2f2] flex lg:h-64 md:h-48 sm:h-36 relative">
-          <div className="absolute md:left-96 lg:ml-32 bg-sky-400 w-96 lg:h-64 md:h-48 overflow-hidden">
+        <div className="bg-[#f2f2f2] flex lg:h-80 md:h-60 sm:h-52 h-44 lg:w-96 md:w-72 sm:w-60 w-56 relative">
+          <div className="absolute bg-sky-400 w-11/12 lg:h-80 md:h-80 sm:h-60 h-44 overflow-hidden">
             <p className="text-sky-400">Secret text</p>
           </div>
-          <video controls className="lg:h-64 md:h-48 sm:h-36 absolute"></video>
-          <div className="lg:left-96 lg:ml-32 md:left-96 md:ml-2 lg:top-0 md:top-4 absolute">
-            <div className="flex lg:mt-6 lg:ml-6 md:mt-3 md:ml-3 sm:mt-1 sm:ml-1">
-              <p className="font-normal text-lg">Event date</p>
-              <p className="mr-2 ml-2 font-normal text-lg"> | </p>
-              <p className="font-normal text-lg">Video length</p>
+          <video controls className="lg:w-full absolute"></video>
+          <div className="lg:left-6 md:left-4 sm:left-3 left-2 lg:top-48 md:top-36 sm:top-28 top-28 lg:mt-2 md:mt-2 sm:mt-3 absolute">
+            <div className="flex">
+              <p className="font-normal lg:text-base md:text-sm sm:text-xs text-xs">Event date</p>
+              <p className="mr-2 ml-2 font-normal lg:text-base md:text-sm sm:text-xs text-xs"> | </p>
+              <p className="font-normal lg:text-base md:text-sm sm:text-xs text-xs">Video length</p>
             </div>
-            <div className="lg:mt-12 lg:ml-6 md:mt-4 md:ml-3 sm:mt-2 sm:ml-1">
-              <p className="font-medium text-3xl">Video Title</p>
+            <div className="lg:top-9 md:top-6 sm:top-6 top-5 lg:w-72 md:w-52 sm:w-44 w-44  absolute">
+              <p className="font-medium lg:text-xl md:text-base sm:text-sm text-sm">Video Title</p>
             </div>
-            <div className="lg:mt-14 lg:ml-6 md:mt-4 md:ml-3 sm:mt-3 sm:ml-2">
-              <p className="font-normal text-lg">Video origin</p>
+            <div className="lg:top-20 md:top-14 sm:top-14 top-11 absolute">
+              <p className="font-normal lg:text-base md:text-sm sm:text-xs text-xs">Video origin</p>
             </div>
           </div>
-          <div className="absolute lg:right-16 lg:top-28 md:hidden">
-            <p className="text-3xl">{(data.votes / totalVotes).toFixed(2)}%</p>
+          <div className="absolute lg:right-6 md:right-4 sm:right-3 right-2 lg:top-60 md:top-44 sm:top-36 top-32 lg:mt-0 md:mt-0 sm:mt-1 mt-1">
+            <p className="lg:text-xl md:text-base sm:text-sm text-sm">{(data.votes / totalVotes).toFixed(2)}%</p>
           </div>
         </div>
       </div>

c2d65101cc2b1a2074f8b13ef7cf6620de548a01
ChrisDTon <christer.daniel@gmail.com>
2023-03-06 12:38:45 +0100

    Testing video src for voting page

--- src/components/VotableItem.tsx
+++ src/components/VotableItem.tsx
@@ -24,7 +24,9 @@ export default function Poll() {
 
   return (
     <div>
-    <video controls className="aspect-video w-full"></video>
+    <video controls className="aspect-video w-full" poster="https://d22hh18o76pkhl.cloudfront.net/mediabank/thumb/eliteserien/6115/05199.jpg">
+      <source src="https://api.forzasys.com/eliteserien/playlist.m3u8/6115:5189000:5214000/Manifest.m3u8" type="application/x-mpegURL"></source>
+    </video>
     <div className="bg-[#f2f2f2] lg:h-32 md:h-28 sm:h-24 h-20 w-full overflow-hidden grid grid-cols-5 gap-0 cursor-pointer hover:bg-gray-200 hover:shadow-lg" onClick={myVote}>
       <div className="col-span-4">
         <div className="absolute bg-sky-400 lg:h-32 md:h-28 sm:h-24 h-20 overflow-hidden" style={{width:384*progress}}>
